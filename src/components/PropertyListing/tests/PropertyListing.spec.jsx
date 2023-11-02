10e1f646ef93552a8fda5a67bd109bfd2db944f0
naren-bellala <narenz@icloud.com>
2023-11-02 11:05:51 +0000

    feat(propertylisting): - Connected to real api with hooks - Fixed the broken tests

--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { within } from '@testing-library/dom';
 import PropertyListing from '../PropertyListing';
 
+const mockCount = 5;
+const mockData = Array(mockCount).fill({
+    id: 73864112,
+    bedrooms: 3,
+    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+    propertyType: 'Flat',
+    price: 1950000,
+    branchName: 'M2 Property, London',
+    propertyUrl: '/property-for-sale/property-73864112.html',
+    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+    propertyTitle: '3 bedroom flat for sale',
+    mainImage:
+        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+});
+
+beforeEach(
+    () =>
+        (global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () => Promise.resolve(mockData),
+            })
+        ))
+);
+
+afterEach(() => global.fetch.mockRestore());
 describe('PropertyListing', () => {
-    it('should render five property cards', async () => {
+    it(`should render ${mockCount} property cards`, async () => {
         render(<PropertyListing />);
-        const propertiesList = screen.getByRole('list');
-        const propertyCards = await within(propertiesList).findAllByRole('listitem');
-        expect(propertyCards).toHaveLength(5);
+        waitFor(async () => {
+            const propertiesList = screen.getByRole('list');
+            const propertyCards = await within(propertiesList).findAllByRole('listitem');
+            expect(propertyCards).toHaveLength(mockCount);
+        });
     });
 });
